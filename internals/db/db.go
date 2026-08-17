7a605b518ad17dc90dab90a31f86b19305e46363
Aasif Khan <aasifkhan.a2006@gmail.com>
2026-08-17 13:31:59 +0530

    feat: establish db connection

---
+++ internals/db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func Connect(databaseUrl string) (*sql.DB, error) {
+
+	db, err := sql.Open("pgx", databaseUrl)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("ping: %w", err)
+	}
+
+	return db, nil
+}
