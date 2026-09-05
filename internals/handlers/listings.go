92c9491f0bfe59f16b75721aeae0a057b20f04dc
Aasif Khan <aasifkhan.a2006@gmail.com>
2026-09-05 13:31:06 +0530

    feat: created listing endpoint

--- internals/handlers/listings.go
+++ internals/handlers/listings.go
@@ -15,7 +15,7 @@ type listing struct {
 	ID          string    `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
-	Price       string    `json:"price"`
+	Price       int64     `json:"price"`
 	City        string    `json:"city"`
 	CreatedAt   time.Time `json:"created_at"`
 }
@@ -93,5 +93,39 @@ func (lh ListingHandler) Delete(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusNoContent)
+
+}
+
+func (lh ListingHandler) Create(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	requestId := middlewares.RequestIDFromContext(ctx)
+
+	var req listing
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		lh.logger.Error("failed to decode", "request_id", requestId, "err", err)
+		httpx.Error(w, http.StatusBadRequest, "invalid body", httpx.CodeMalformedJSON)
+		return
+	}
+
+	row := lh.db.QueryRowContext(ctx, `
+		INSERT INTO listings (title, description, price, city) 
+		VALUES ($1, $2, $3, $4) 
+		RETURNING id`, req.Title, req.Description, req.Price, req.City)
+
+	var id string
+	if err = row.Scan(&id); err != nil {
+		lh.logger.Error("failed to scan", "request_id", requestId, "err", err)
+		httpx.Error(w, http.StatusInternalServerError, "something went wrong", httpx.CodeInternalError)
+		return
+	}
+
+	lh.logger.Info("listing created", "request_id", requestId, "listing_id", id)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	_ = json.NewEncoder(w).Encode(map[string]string{"id": id})
 
 }
