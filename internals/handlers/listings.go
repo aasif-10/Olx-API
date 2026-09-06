2f0b36f662a97da939f65dd9b388ee49c7af21e8
Aasif Khan <aasifkhan.a2006@gmail.com>
2026-09-06 21:37:29 +0530

    feat: created dtos for create listing endpoint

--- internals/handlers/listings.go
+++ internals/handlers/listings.go
@@ -100,7 +100,7 @@ func (lh ListingHandler) Create(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	requestId := middlewares.RequestIDFromContext(ctx)
 
-	var req listing
+	var req CreateListingRequest
 
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
@@ -112,20 +112,20 @@ func (lh ListingHandler) Create(w http.ResponseWriter, r *http.Request) {
 	row := lh.db.QueryRowContext(ctx, `
 		INSERT INTO listings (title, description, price, city) 
 		VALUES ($1, $2, $3, $4) 
-		RETURNING id`, req.Title, req.Description, req.Price, req.City)
+		RETURNING id, title, created_at`, req.Title, req.Description, req.Price, req.City)
 
-	var id string
-	if err = row.Scan(&id); err != nil {
+	var out CreateListingResponse
+	if err = row.Scan(&out.ID, &out.Title, &out.CreatedAt); err != nil {
 		lh.logger.Error("failed to scan", "request_id", requestId, "err", err)
 		httpx.Error(w, http.StatusInternalServerError, "something went wrong", httpx.CodeInternalError)
 		return
 	}
 
-	lh.logger.Info("listing created", "request_id", requestId, "listing_id", id)
+	lh.logger.Info("listing created", "request_id", requestId, "listing_id", out.ID)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
-	_ = json.NewEncoder(w).Encode(map[string]string{"id": id})
+	_ = json.NewEncoder(w).Encode(out)
 
 }
