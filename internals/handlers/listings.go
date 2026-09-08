c3f83ea524624c0bb518dbe12a6bb99a3b55777c
Aasif Khan <aasifkhan.a2006@gmail.com>
2026-09-08 23:10:22 +0530

    routes validatino check

--- internals/handlers/listings.go
+++ internals/handlers/listings.go
@@ -109,7 +109,7 @@ func (lh ListingHandler) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	 := lh.db.QueryRowContext(ctx, `
+	row := lh.db.QueryRowContext(ctx, `
 		INSERT INTO listings (title, description, price, city) 
 		VALUES ($1, $2, $3, $4) 
 		RETURNING id, title, created_at`, req.Title, req.Description, req.Price, req.City)
